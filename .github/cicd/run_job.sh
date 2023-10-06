54475d3611a3311e686739018f52bb6f21f682da
jhchein <hehein@microsoft.com>
2023-10-06 18:39:48 +0200

    added required workspace name and resource group

--- .github/cicd/run_job.sh
+++ .github/cicd/run_job.sh
@@ -3,13 +3,13 @@ if [[ -z "$2" ]]
   then
     if [[ "$job" =~ pipeline.yml ]]
     then
-      run_id=$(az ml job create -f $job --query name -o tsv --set settings.force_rerun=True)
+      run_id=$(az ml job create -f $job --resource-group $RESOURCE_GROUP_NAME --workspace-name $WORKSPACE_PARAM --query name -o tsv --set settings.force_rerun=True)
     else
-      run_id=$(az ml job create -f $job --query name -o tsv)
+      run_id=$(az ml job create -f $job --resource-group $RESOURCE_GROUP_NAME --workspace-name $WORKSPACE_PARAM --query name -o tsv)
     fi
   else
     experiment_name=$2
-    run_id=$(az ml job create -f $job --query name -o tsv --set experiment_name=$experiment_name --set settings.force_rerun=True)
+    run_id=$(az ml job create -f $job --resource-group $RESOURCE_GROUP_NAME --workspace-name $WORKSPACE_PARAM --query name -o tsv --set experiment_name=$experiment_name --set settings.force_rerun=True)
 fi
 
 if [[ -z "$3" ]]
